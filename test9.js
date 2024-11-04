b14d8dc36fb294b713e23a8e75dfb1e6e3fc3e23
Seikixo <150346850+Seikixo@users.noreply.github.com>
2024-11-04 14:00:33 +0800

    test 9

---
+++ test9.js
@@ -0,0 +1,9 @@
+function flattenArray(arr){
+    return arr.reduce(
+        (flat, toFlatten) =>
+            flat.concat(Array.isArray(toFlatten) ? flattenArray(toFlatten) : toFlatten),
+        []
+    );
+}
+
+console.log(flattenArray([1,[2, [3, [4]], 5]]));
